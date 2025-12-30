b2bbb8757e499ad3e91ba4832b907028464598c1
fassiliss <fassiliss@gmail.com>
2025-12-30 07:59:56 -0600

    Components

--- src/Components/PackingList.jsx
+++ src/Components/PackingList.jsx
@@ -1,11 +1,39 @@
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+import { useState } from "react";
+
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+    const [sortBy, setSortBy] = useState("input");
+    let sortedItems;
+    if (sortBy === "description") {
+      sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+    } else if (sortBy === "packed") {
+      sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+    } else {
+      sortedItems = items;
+    }
+     if (sortBy === "quantity") {
+      sortedItems = items.slice().sort((a, b) => a.quantity - b.quantity);
+    } else {
+      sortedItems = items;
+    }
+
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id} />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+           <option value="input">Sort by...</option>
+           <option value="description">Description</option>
+           <option value="packed">Packed status</option>
+           <option value="quantity">Quantity</option>
+        </select>
+        <button onClick={() => onClearList()}>Clear list</button>
+      </div>
     </div>
   );
 }
